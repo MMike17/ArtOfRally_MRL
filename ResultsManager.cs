6ce37a82d67411f284b52502859aa43835898631
MMike17 <matthews.r.mike@gmail.com>
2026-08-22 16:04:06 +0200

    Add results file encryption

--- ResultsManager.cs
+++ ResultsManager.cs
@@ -7,6 +7,7 @@ namespace MRL
     class ResultsManager
     {
         const string RESULTS_FILE_NAME = "RallyResults.txt";
+        const int ENCRYPTION_KEY = 573; // TODO : This will get changed with rolling encryption
 
         public static bool IsRecording { get; private set; }
 
@@ -20,16 +21,22 @@ namespace MRL
         {
             string filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory), RESULTS_FILE_NAME);
 
-            File.WriteAllText(
-                filePath,
-                JsonUtility.ToJson(new EventResults(playerResults), true)
-            );
-
+            File.WriteAllText(filePath, EncryptResults(new EventResults(playerResults)));
             Main.Log("Saved rally results to " + filePath);
-            // TODO : Open folder to file path ?
 
+            // TODO : Open folder to file path ?
             IsRecording = false;
-            Main.Log("Reset results recording");
+        }
+
+        private static string EncryptResults(EventResults results)
+        {
+            string jsonData = JsonUtility.ToJson(results, true);
+            string result = string.Empty;
+
+            for (int i = 0; i < jsonData.Length; i++)
+                result += ((char)(jsonData[i] ^ ENCRYPTION_KEY));
+
+            return result;
         }
     }
 }
