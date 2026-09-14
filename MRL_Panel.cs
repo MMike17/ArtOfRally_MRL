570950430b6ff8ee24a36d4a2894daf98c2ec77e
MMike17 <matthews.r.mike@gmail.com>
2026-09-14 12:11:14 +0200

    Change flag placement

--- MRL_Panel.cs
+++ MRL_Panel.cs
@@ -7,18 +7,16 @@ using UnityEngine.EventSystems;
 using UnityEngine.UI;
 using static AreaManager;
 using static Car;
-using static StyleText;
 
 namespace MRL
 {
     /// <summary>Screen that displays infos about MRL events</summary>
     public class MRL_Panel : Panel
     {
-        private const string STYLE_PROP_NAME = "_scaleType";
-
         private Button mainButton;
-        private Image flagImage;
         private Text titleText;
+        private Image flagImage;
+        private Text subTitleText;
         private Text stagesText;
         private Image panelImage;
         private Text deadlineText;
@@ -28,8 +26,9 @@ namespace MRL
 
         public void Setup(Font boldFont, Font standardFont, Action PopCarChoicePanel)
         {
-            flagImage = transform.GetChild(0).GetChild(0).GetChild(0).GetComponent<Image>();
-            titleText = transform.GetChild(0).GetChild(1).GetComponent<Text>();
+            titleText = transform.GetChild(0).GetChild(0).GetComponent<Text>();
+            flagImage = transform.GetChild(0).GetChild(1).GetChild(0).GetComponent<Image>();
+            subTitleText = transform.GetChild(0).GetChild(2).GetComponent<Text>();
             stagesText = transform.GetChild(1).GetComponent<Text>();
             panelImage = transform.GetChild(2).GetChild(0).GetComponent<Image>();
             deadlineText = transform.GetChild(3).GetComponent<Text>();
@@ -38,28 +37,28 @@ namespace MRL
             UIScale uiScale = StyleManager.Instance().UIScale;
 
             titleText.font = boldFont;
-            StyleText style = titleText.gameObject.AddComponent<StyleText>();
-            Main.SetField(style, STYLE_PROP_NAME, BindingFlags.Instance, TextType.StageTitle);
-            titleText.fontSize = Mathf.RoundToInt(Mathf.Lerp(
+            titleText.gameObject.AddComponent<StyleText>();
+            titleText.fontSize = Mathf.RoundToInt(StyleConstants.Text.StageTitle.GetFontSize(uiScale));
+
+            subTitleText.font = boldFont;
+            subTitleText.gameObject.AddComponent<StyleText>();
+            subTitleText.fontSize = Mathf.RoundToInt(Mathf.Lerp(
                 StyleConstants.Text.StageTitle.GetFontSize(uiScale),
                 StyleConstants.Text.Header1.GetFontSize(uiScale),
-                0.5f
+                0.66f
             ));
 
             stagesText.font = standardFont;
-            style = stagesText.gameObject.AddComponent<StyleText>();
-            Main.SetField(style, STYLE_PROP_NAME, BindingFlags.Instance, TextType.Header1);
+            stagesText.gameObject.AddComponent<StyleText>();
             stagesText.fontSize = StyleConstants.Text.Header1.GetFontSize(uiScale);
             stagesText.lineSpacing = 1.5f;
 
             deadlineText.font = boldFont;
-            style = deadlineText.gameObject.AddComponent<StyleText>();
-            Main.SetField(style, STYLE_PROP_NAME, BindingFlags.Instance, TextType.StageTitle);
+            deadlineText.gameObject.AddComponent<StyleText>();
             deadlineText.fontSize = StyleConstants.Text.StageTitle.GetFontSize(uiScale);
 
             descriptionText.font = standardFont;
-            style = descriptionText.gameObject.AddComponent<StyleText>();
-            Main.SetField(style, STYLE_PROP_NAME, BindingFlags.Instance, TextType.Standard);
+            descriptionText.gameObject.AddComponent<StyleText>();
             descriptionText.fontSize = StyleConstants.Text.Standard.GetFontSize(uiScale);
 
             mainButton = transform.GetChild(5).GetComponent<Button>();
@@ -105,11 +104,12 @@ namespace MRL
             this.isSeason = isSeason;
             ServerInfo infos = CustomEventManager.serverInfos;
 
+            titleText.text = $"{infos.currentRally.name}";
+            subTitleText.text = $"{infos.currentRally.country} - {infos.currentSeason.year} - " +
+                $"{(isSeason ? "Season" : "Open class")} rally";
+
             Country country = Countries.GetCountryByPartialFullName(infos.currentRally.country)[0];
             flagImage.sprite = Resources.Load<Sprite>($"Sprites/CountryFlags/{country.Alpha2}");
-
-            titleText.text = $"{infos.currentRally.name}\n" +
-                $"{infos.currentRally.country} - {infos.currentSeason.year} - {(isSeason ? "Season" : "Open class")} rally";
 
             Dictionary<Areas, Area> areaDictionary = Main.GetField<Dictionary<Areas, Area>, AreaManager>(
                 null,
